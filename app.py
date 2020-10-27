386166a85ae213287917d1a0a907869a8b0900e5
Eugenio Pacelli Jr <pacellijr@gmail.com>
2020-10-27 14:14:39 -0300

    configurando a porta correta

--- app.py
+++ app.py
@@ -20,5 +20,6 @@ def home():
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
